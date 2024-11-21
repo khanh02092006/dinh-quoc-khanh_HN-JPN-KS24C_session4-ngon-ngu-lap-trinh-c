bff4c9280e04c1404f08d1659332f5cf20b06389
khanh02092006 <dinhkhanh1881@gmail.com>
2024-11-21 22:16:16 +0700

    session4_ex1.

---
+++ session4_ex1.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+	int a;
+	printf("nhap vao 1 so nguyen ");
+	scanf("%d",&a);
+	if(a>0){
+		printf("so nguyen là duong ");
+		} else {
+			printf(" so nguyen la am ");
+			}
+			return 0;
+			}
+		
+
